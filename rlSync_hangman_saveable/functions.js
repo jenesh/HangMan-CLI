ad854fd11a32b7b1970515537a89768569afd2a6
Jenesh <jenesh.napit@gmail.com>
2019-08-15 14:27:14 -0400

    made minor changes

--- rlSync_hangman_saveable/functions.js
+++ rlSync_hangman_saveable/functions.js
@@ -1,24 +1,27 @@
 // Splits string word into an array word
 const splitWordToArr = str => str.split("");
 
-// 
+// Makes new board array
 const makeBoardArr = str => str.map(ele => ele);
 
 // Updates the given letter between two arrays
-const updateDynamicArr = (arr, ltr) => arr.map(ele => (ele.toLowerCase() === ltr ? `${ele.toUpperCase()}` : ele));
+const updateDynamicArr = (arr, ltr) =>
+  arr.map(ele => (ele.toLowerCase() === ltr ? `${ele.toUpperCase()}` : ele));
 // console.log(arr.map(ele => (ele === "ltr" ? true : false)));
 
 // Updates static array with a $
-const updateStaticArr = (arr, ltr) => arr.map(ele => (ele === ltr ? "$" : `${ele}`));
+const updateStaticArr = (arr, ltr) =>
+  arr.map(ele => (ele === ltr ? "$" : `${ele}`));
 
 // Checks if input is a valid letter
-const letterCheck = (char) => char.match(/[a-z]/i);
+const letterCheck = char => char.match(/[a-z]/i);
 
 // Display's ("_")'s if the board array elements are lower case
-const displayBoardArr = arr => arr.map(ele => ele === ele.toLowerCase() ? '_' : ele);
+const displayBoardArr = arr =>
+  arr.map(ele => (ele === ele.toLowerCase() ? "_" : ele));
 
 // Check if every element is '$'
-const isSolved = arr => arr.every(ele => ele === '$');
+const isSolved = arr => arr.every(ele => ele === "$");
 
 module.exports = {
   splitWordToArr: splitWordToArr,
